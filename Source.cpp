572e79e630029811ade0b21d13b8de2171f8a76d
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-10-14 18:31:11 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,7 @@ int main(int argc, char ** argv) {
 	try {
 		
 		Szorny s1 = Szorny::parseUnit(argv[2]);
-		Szorny s2=  Szorny::parseUnit(argv[2]);
+		Kalandor s2(Szorny::parseUnit(argv[2]));
 		//while (s1.getHp() > 0 && s2.getHp() > 0) {=
 			//s1.tamad(s2);
 			//if (s2.getHp() > 0) {
