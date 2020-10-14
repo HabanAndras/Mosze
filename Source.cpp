9c4a38811c8f4e5f44127bffc0af836f6b7d95a4
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-10-14 14:28:49 +0200

    Faszkiwan

---
+++ Source.cpp
@@ -0,0 +1,32 @@
+#include "Szorny.h"
+#include "Kalandor.h"
+
+#include <string>
+
+int main(int argc, char ** argv) {
+	try {
+		Kalandor s1 = Kalandor::parseUnit(argv[1]);
+		Szorny s2 = Szorny::parseUnit(argv[2]);
+		while (s1.getHp() > 0 && s2.getHp() > 0) {
+			s1.tamad(s2);
+			if (s2.getHp() > 0) {
+				s2.tamad(s1);
+			}
+		}
+		if (s1.getHp() == 0) std::cout << s2.getName() << " wins. Remaining HP: " << s2.getHp() << std::endl;
+		if (s2.getHp() == 0) std::cout << s1.getName() << " wins. Remaining HP: " << s1.getHp() << ", current level: " << s1.getLvl() << ", current experience: " << s1.getXp() << std::endl;
+
+		return 0;
+
+	}
+     catch (int e) {
+        switch(e) {
+            case 56: std::cout << "File does not exist!" << std::endl;
+                break;
+            case 57: std::cout << "Invalid character value!!" << std::endl;
+                break;
+        }
+		return 0;
+    }
+}
+
