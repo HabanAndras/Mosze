191a8c36a49bf95cadd573fc67778de514e368da
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-12-02 14:10:21 +0100

    proba

--- tests/test.cpp
+++ tests/test.cpp
@@ -176,8 +176,10 @@ Game jatek;
 Damage d;
 d.physical = 3;
 d.magical = 1;
+Monster monster1("Sotetvarazslo", 250, d, 1, 2.0);
 Hero hos("Prince Aidan of Khanduras", 30, d, 1, 1.1, 20, 5, 1, 1, 1, 0.9, 1, 1);
 ASSERT_THROW(jatek.putHero(hos,1,1), Map::WrongIndexException);
+ASSERT_THROW(jatek.putMonster(monster1,1,3), Map::WrongIndexException);
 }
 
 TEST(GameTest,Only_one_hero_test){
@@ -192,6 +194,19 @@ Hero hos2("Hosarkany", 30, d, 1, 1.1, 20, 5, 1, 1, 1, 0.9, 1, 1);
 jatek.putHero(hos,1,1);
 ASSERT_THROW(jatek.putHero(hos2,1,3), Game::AlreadyHasHeroException);
 }
+
+TEST(GameTest,Monster_Hero_Wall_test){
+Game jatek;
+Damage d;
+d.physical = 3;
+d.magical = 1;
+Map palya("palya1.txt");
+jatek.setMap(palya);
+Hero hos("Prince Aidan of Khanduras", 30, d, 1, 1.1, 20, 5, 1, 1, 1, 0.9, 1, 1);
+ASSERT_THROW(jatek.putHero(hos,1,0), Game::OccupiedException);
+}
+
+
 
 
 int main(int argc, char ** argv) {
