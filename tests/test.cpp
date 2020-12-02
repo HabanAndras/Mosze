917efcbfb6bd0b0d10482a6e9ee7a003c1f7ccc8
HabanAndras <56293260+HabanAndras@users.noreply.github.com>
2020-12-02 13:22:51 +0100

    proba

--- tests/test.cpp
+++ tests/test.cpp
@@ -73,8 +73,8 @@ EXPECT_EQ(vart, output);
 TEST(Maptest, Getter_test) {
 	ASSERT_NO_THROW(Map("palya1.txt"));
 	Map palya("palya1.txt");
-	EXPECT_EQ(palya.get(4,1),1);
-	EXPECT_EQ(palya.get(4, 0), 0);
+	EXPECT_EQ(palya.get(1,1),1);
+	EXPECT_EQ(palya.get(5,5), 0);
 }
 
 TEST(unittests, Exceptions_test) {
